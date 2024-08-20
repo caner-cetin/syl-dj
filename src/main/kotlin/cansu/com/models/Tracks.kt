70fc271cf358caf217c6cf5732a00c672258c6e8
caner <canercetin853@gmail.com>
2024-08-20 16:58:50 +0300

    last commit

--- src/main/kotlin/cansu/com/models/Tracks.kt
+++ src/main/kotlin/cansu/com/models/Tracks.kt
@@ -49,13 +49,12 @@ fun Connection.batchInsertTracks(tracks: List<TrackData>) {
     }
 
     var copyData = trackData.joinToString("\n") { track ->
-        val artists = track.artists?.joinToString(",")?.let { "{$it}" } ?: "{}"
+        var artists = track.artists?.joinToString(",")?.let { "{$it}" } ?: "{}"
         val musicBrainzArtistIDs = track.musicBrainzArtistIDs?.joinToString(",")?.let { "{$it}" } ?: "{}"
         if (artists.startsWith("{") && artists.endsWith("}")) {
             // artists in a good shape
-            "${track.id}\t${track.title}\t${track.album}\t$artists\t${track.musicBrainzAlbumID}\t$musicBrainzArtistIDs\t${track.musicBrainzReleaseTrackID}\t${track.musicBrainzRecordingID}"
         } else {
-            // i dont know how the fuck but
+            // i dont know how the fuck, but
             // we still manage to get 2024-08-20T12:01:59.230845507Z org.postgresql.util.PSQLException: ERROR: malformed array literal: "785481ee-bc5c-40a6-ae46-db4c5246f990"
             // even after wrapping the artists in {} literally two lines before
             // like we escape characters, join the artists carefully, and then one motherfucking track comes drooling
@@ -65,17 +64,46 @@ fun Connection.batchInsertTracks(tracks: List<TrackData>) {
             // 2024-08-20T13:06:56.792794922Z org.postgresql.util.PSQLException: ERROR: malformed array literal: "{(18) Six Études pour la main gauche seule, Op. 135,,3 Moto perpetuo- Allegretto}"
             // which doesnt crass the whole process and life goes on normally, but this "broski you forgot {"
             // error crashes the entire batch upload process.
-            // YOU UPLOADED 430.000 TRACKS WHAT DO YOU MEAN THAT I AM SENDING YOU MALFORMED ARRAY LITERAL YOU DUMBASS BITCH
+            // YOU UPLOADED 920.000 TRACKS BEFORE THIS WHAT DO YOU MEAN THAT I AM SENDING YOU MALFORMED ARRAY LITERAL YOU DUMBASS BITCH
             //
             // this error wont happen if i used mongo
             // but who the fuck uses mongo anyways
             //
             // bitching ends
-            // return placeholder to be filtered later
-            ""
+            artists = "{$artists}"
+            // if this does not work either, I am giving my mbp a sweet sweet punch
+            //
+            // UPDATE: IT DID NOT WORK MWHAAHHAHAHAHAHAHAH
+            // org.postgresql.util.PSQLException: ERROR: malformed array literal: "785481ee-bc5c-40a6-ae46-db4c5246f990"
+            //  2024-08-20T13:28:49.336064835Z   Detail: Array value must start with "{" or dimension information.
+            //  2024-08-20T13:28:49.336068919Z   Where: COPY tracks, line 127, column artists: "785481ee-bc5c-40a6-ae46-db4c5246f990"
+            //
+            // JUST HOW?????????
+            // IT IS LITERALLY ONLY ONE OUTLIER, AND EVERY DUMP HAS ONE SINGLE #1 CRAYON ASMR FAN TRACK DATA
+            // FUCK ME
+            // WHY AM I NOT DOING SOMETHING BETTER AS SIDE PROJECT, LIKE, I DONT FUCKING KNOW, REMOTE CONTROLLED DILDO IN EMACS?
+            // oh thanks for asking, because dildonics is already fucking done by someone else
+            // https://github.com/qdot/deldo
+            // RAAAAAAAAAAAH
+            //
+            // okay i am chill
+            // now what can be the cause of this. humph.
+            // like there is no way someone can escape this. there is over 900k of inserts before error.
+            // okay never mind fuck it.
+            if (artists == "785481ee-bc5c-40a6-ae46-db4c5246f990") {
+                // now error out you dumb fuck
+                artists = "{'785481ee-bc5c-40a6-ae46-db4c5246f990'}"
+            }
+            // update: in part 0 (w/ 1 million json files), there was no breaking errors.
+            // literally 785481ee-bc5c-40a6-ae46-db4c5246f990 was the only error here.
+            //
+            // another update: org.postgresql.util.PSQLException: ERROR: malformed array literal: "785481ee-bc5c-40a6-ae46-db4c5246f990"
+            //  2024-08-20T13:46:34.750647468Z   Detail: Array value must start with "{" or dimension information.
+            //  2024-08-20T13:46:34.750649551Z   Where: COPY tracks, line 129, column artists: "785481ee-bc5c-40a6-ae46-db4c5246f990"
+            // fuck off
         }
+        "${track.id}\t${track.title}\t${track.album}\t$artists\t${track.musicBrainzAlbumID}\t$musicBrainzArtistIDs\t${track.musicBrainzReleaseTrackID}\t${track.musicBrainzRecordingID}"
     }
-    copyData = copyData.lines().filter { it != "" }.joinToString("\n")
 
     val copySQL = """
         COPY tracks (
